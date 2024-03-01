b0a44ece420716059fe8c577911d3a91cd202dae
d33bs <dave.bunten@cuanschutz.edu>
2024-03-01 14:19:54 -0700

    remove key file

--- terraform/operations/accounts.tf
+++ terraform/operations/accounts.tf
@@ -4,8 +4,3 @@ resource "google_service_account" "service_account" {
   account_id = "${var.initiative_label}-svc-account"
 }
 
-#Create a service-account key for the associated service account
-resource "google_service_account_key" "key" {
-  service_account_id = google_service_account.service_account.name
-  public_key_type    = "TYPE_X509_PEM_FILE"
-}
