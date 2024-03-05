dcff03747e16ad1d32077c9b950345813a10f98c
d33bs <dave.bunten@cuanschutz.edu>
2024-03-05 15:11:28 -0700

    avoid character limits with svc account name

--- terraform/operations/accounts.tf
+++ terraform/operations/accounts.tf
@@ -1,5 +1,5 @@
 # tf account creation and related work
 # Create a new service account
 resource "google_service_account" "service_account" {
-  account_id = "${var.initiative_label}-svc-account"
+  account_id = "waylab-em-tokyo-svc-account"
 }
